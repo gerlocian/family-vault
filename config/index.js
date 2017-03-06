b1557eee06d118de1164644bc0200b8c18e91f60
Patrick <pogithub@gmail.com>
2017-03-06 00:30:14 -0500

    Added command line argument for dev mode

--- config/index.js
+++ config/index.js
@@ -1,5 +1,9 @@
 'use strict';
 
+import { argv } from 'yargs';
+
+if (argv.development) process.env.LOGGER_LEVEL = 'debug';
+
 export const SERVER_PORT = process.env.SERVER_PORT || 3000;
 export const LOGGER_LEVEL = process.env.LOGGER_LEVEL || 'info';
 export const MORGAN_CONF = process.env.MORGAN_CONF || 'combined';
